a0752e8f21e2974af5b13f3c33736bdb15c72254
Jhonatan <jvasconcelosbittencourt@gmail.com>
2026-07-15 14:30:05 -0300

    iniciando postagens

--- vue-project/src/components/PostsView.vue
+++ vue-project/src/components/PostsView.vue
@@ -1,9 +1,16 @@
 <script lang="ts">
 export default {
   data() {
+    const localPosts = localStorage.getItem('postsVue')
     return {
-      posts: false,
+      posts: localPosts
+        ? (JSON.parse(localPosts) as { title: string; text: string; id: number }[])
+        : [],
       user: localStorage.getItem('nameVue'),
+      showAside: false,
+      titleVue: '',
+      postVue: '',
+      number: 0,
     }
   },
   methods: {
@@ -12,6 +19,20 @@ export default {
       localStorage.removeItem('emailVue')
       this.user = null
       window.location.reload()
+    },
+    asideShow() {
+      this.showAside = !this.showAside
+    },
+    storeText() {
+      this.posts.push({
+        title: this.titleVue,
+        text: this.postVue,
+        id: (this.number += 1),
+      })
+      localStorage.setItem('postsVue', JSON.stringify(this.posts))
+    },
+    showPost(text: string) {
+      // console.log(text)
     },
   },
 }
@@ -30,30 +51,89 @@ export default {
           </div>
         </div>
 
-        <div class="block is-flex is-justify-content-space-between">
-          <p class="title">Posts</p>
-          <button type="button" class="button is-link">Add New Post</button>
-        </div>
+        <div class="is-flex">
+          <div class="is-flex-grow-1">
+            <div class="block is-flex is-justify-content-space-between">
+              <p class="title">Posts</p>
+              <button type="button" class="button is-link" @click="asideShow">Add New Post</button>
+            </div>
 
-        <table class="table is-fullwidth is-striped is-hoverable is-narrow" v-if="posts">
-          <thead>
-            <tr class="has-background-link-light">
-              <th>ID</th>
-              <th>Title</th>
-              <th class="has-text-right">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>id</td>
-              <td>title</td>
-              <td class="has-text-right is-vcentered">
-                <button type="button" class="button is-link">Open</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <p class="has-text-centered" v-else>No posts yet.</p>
+            <table
+              class="table is-fullwidth is-striped is-hoverable is-narrow"
+              v-if="posts.length > 0"
+            >
+              <thead>
+                <tr class="has-background-link-light">
+                  <th>ID</th>
+                  <th>Title</th>
+                  <th class="has-text-right">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="post in posts" :key="post.id">
+                  <td>{{ post.id }}</td>
+                  <td>{{ post.title }}</td>
+                  <td class="has-text-right is-vcentered">
+                    <button type="button" class="button is-link" @click="showPost(post.text)">
+                      Open
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <p class="has-text-centered" v-else>No posts yet.</p>
+          </div>
+
+          <div
+            class="is-flex is-flex-direction-column"
+            :style="{
+              width: showAside ? '500px' : '0px',
+              height: showAside ? 'auto' : '0px',
+              minWidth: showAside ? '500px' : '0px',
+              marginLeft: showAside ? '1.5rem' : '0px',
+              paddingLeft: showAside ? '20px' : '0px',
+              gap: showAside ? '15px' : '0px',
+              overflow: 'hidden',
+              transition: '0.4s',
+            }"
+          >
+            <h2>Create new posts</h2>
+
+            <form @submit="storeText">
+              <div class="is-flex is-flex-direction-column" style="gap: 10px">
+                <label for="titleVue">Title </label>
+                <input
+                  type="text"
+                  name="titleVue"
+                  id="titleVue"
+                  placeholder="Post Title"
+                  class="input"
+                  v-model="titleVue"
+                  required
+                />
+                <label for="postVue">Write Post Body</label>
+                <textarea
+                  class="input"
+                  placeholder="Post body"
+                  id="postVue"
+                  name="postVue"
+                  v-model="postVue"
+                  required
+                ></textarea>
+              </div>
+              <div class="field is-grouped mt-5">
+                <div class="control">
+                  <button type="submit" class="button is-link">Create</button>
+                </div>
+                <div class="control">
+                  <button type="button" class="button is-link is-light" @click="asideShow">
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   </div>
